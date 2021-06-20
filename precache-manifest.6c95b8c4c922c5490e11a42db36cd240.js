e6b04ed31495a0889d0d355a1d4bccf812d22db9
Skchkov <skchkov@yandex.ru>
2021-06-20 13:54:51 +0300

    Updates

---
+++ precache-manifest.6c95b8c4c922c5490e11a42db36cd240.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e7be84113c7cc0363bde16b7c0955c",
+    "url": "/Incubator-homeworks/index.html"
+  },
+  {
+    "revision": "088bfcba55d127ad5e5e",
+    "url": "/Incubator-homeworks/static/css/main.35b0f262.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/Incubator-homeworks/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Incubator-homeworks/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "088bfcba55d127ad5e5e",
+    "url": "/Incubator-homeworks/static/js/main.31635271.chunk.js"
+  },
+  {
+    "revision": "0b10cbd1f5685868bc62",
+    "url": "/Incubator-homeworks/static/js/runtime-main.3aa18ba5.js"
+  }
+]);
